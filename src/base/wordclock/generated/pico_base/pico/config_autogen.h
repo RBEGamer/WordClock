b52740f4c2d12e154de6883b83a226d0b64e7cff
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2022-12-24 12:51:51 +0100

    .

--- src/base/wordclock/generated/pico_base/pico/config_autogen.h
+++ src/base/wordclock/generated/pico_base/pico/config_autogen.h
@@ -4,8 +4,8 @@
 
 // based on PICO_CONFIG_HEADER_FILES:
 
-#include "/Users/marcelochsendorf/Downloads/WordClockNew/src/base/wordclock/board/WORDCLOCK_V2_RP2040.h"
+#include "/Users/marcelochsendorf/Downloads/WordClock/src/base/wordclock/board/WORDCLOCK_V2_RP2040.h"
 
 // based on PICO_RP2040_CONFIG_HEADER_FILES:
 
-#include "/Users/marcelochsendorf/Downloads/WordClockNew/src/base/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+#include "/Users/marcelochsendorf/Downloads/WordClock/src/base/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
