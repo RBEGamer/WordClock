775f2de6b3c8593790ee8b3ea9c04bb61dccf10b
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-01-15 17:42:16 +0100

    build 3.2.0

---
+++ src/base/wordclock/build_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir -p ./build_u2f
+for filename in ./board/*.h; do
+        #./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
+        rm WORDCLOCK_*.*
+        rm *.uf2
+        cmake . -DCMAKE_BUILD_TYPE=Release -DWORDCLOCK_BOARD=$(basename "$filename" .h)  && make -j4
+        cp -f *.uf2 ./build_u2f
+done
+
+
+for filename in ./board/*.h; do
+        #./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
+        rm WORDCLOCK_*.*
+        rm *.uf2
+        cmake . -DCMAKE_BUILD_TYPE=Debug -DWORDCLOCK_BOARD=$(basename "$filename" .h)  && make -j4
+        cp -f *.uf2 ./build_u2f
+done
